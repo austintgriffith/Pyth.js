a299e8aad7e953589ae0b96a07729c9e795f71e3
Austin <austin.griffith@madwiremedia.com>
2017-10-31 06:55:49 -0600

    responses added but not staked

--- examples/rawRequestMiner.js
+++ examples/rawRequestMiner.js
@@ -2,6 +2,7 @@ const Request = require('request');
 
 let concurrence = require("../concurrence.js")
 concurrence.init({},(err)=>{
+  concurrence.selectAccount(1)
   concurrence.listRequests().then((requests)=>{
     for(let r in requests){
       let id = requests[r].returnValues.id
@@ -10,21 +11,49 @@ concurrence.init({},(err)=>{
       let protocol = requests[r].returnValues.protocol
       let callback = requests[r].returnValues.callback
       console.log("Found request "+id)
-      console.log("Checking combiner "+combiner);
-      concurrence.isCombinerOpen(id,combiner).then((open)=>{
-        if(open){
-          console.log("Mining request "+request)
-          try{
-            request = JSON.parse(request)
-            console.log(request)
-            Request(request.url,(error, response, body)=>{
-              console.log("body:",body)
-            })
-          }catch(e){console.log(e)}
+
+      console.log("First let's check if the combiner is ready...")
+      concurrence.isCombinerReady(id,combiner).then((ready)=>{
+
+        if(ready){
+          console.log("Combiner "+combiner+" is ready to combine.")
+          concurrence.combine(id,combiner).on('error',(err)=>{
+            if(err){
+              console.log(err)
+            }else{
+              concurrence.getCombinerMode(id,combiner).then((mode)=>{
+                console.log("Combiner Mode: ",mode)
+              })
+            }
+
+          })
+
         }else{
-          console.log("Maybe we can combine?")
+          console.log("Combiner "+combiner+" is not ready to combine, let's see if it's open...");
+          concurrence.isCombinerOpen(id,combiner).then((open)=>{
+            if(open){
+              console.log("Mining request "+request)
+              try{
+                request = JSON.parse(request)
+                console.log(request)
+                Request(request.url,(error, response, body)=>{
+                  console.log("Adding Response:",body)
+                  concurrence.addResponse(id,body).then((result)=>{
+                    console.log("TX:"+result.transactionHash)
+                    console.log(result.events.AddResponse.returnValues)
+                  })
+                })
+              }catch(e){console.log(e)}
+            }else{
+              console.log("Maybe we can combine?")
+              //isCombinerReady
+            }
+          })
         }
+
       })
+
+
 
     }
 
